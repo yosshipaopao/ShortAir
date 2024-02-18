a23f0e4f2862791742d4399f4e0dbd7dbf7a2bb0
yosshipaopao <yosshipaopao@gmail.com>
2024-02-18 21:53:27 +0900

    init

---
+++ App.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using System.Threading;
+using Windows.Storage;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ShortAir
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        public string DEVICE_NAME = "ESP32";
+        public string SERVICE_UUID = "b3f66084-240e-43b6-82c9-96709419da02";
+        public string CHARACTERISTIC_UUID = "4d5d324b-c984-4b44-aa21-7e7bf15058d3";
+        /// <summary>
+        /// 0:not connected ,1: connected 
+        /// </summary>
+        public bool bluethoothConnected = false;
+        public string bluetoothStatus = "";
+        public ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+        public ApplicationDataCompositeValue keyConfig = new ApplicationDataCompositeValue();
+
+
+        Mutex mutex = new Mutex(false, "ShortAir");
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            if (!mutex.WaitOne(0, false))
+            {
+                mutex.Close();
+                Exit();
+            }
+            else
+            {
+                this.InitializeComponent();
+            }
+        }
+        public void onValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            Stream s = args.CharacteristicValue.AsStream();
+            List<int> data = new List<int>();
+            int d = 0;
+            while ((d = s.ReadByte()) != -1)
+            {
+                data.Add(d);
+            }
+            Debug.WriteLine(data.ToArray());
+        }
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            MainWindow m_window = new MainWindow(this);
+            if (!bluethoothConnected) m_window.Activate();
+        }
+
+        public void updateKeyConfig(string receive, string atction)
+        {
+            keyConfig[receive] = atction;
+            localSettings.Values["keyConfig"] = keyConfig;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Timers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Bluetooth;
+using System.Diagnostics;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ShortAir
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        App mainApp ;
+
+        public MainWindow(App app)
+        {
+            this.InitializeComponent();
+            this.mainApp = app;
+        }
+        public void console(string text) { 
+            consoleText.Text = text;
+            Debug.WriteLine(text);
+        }
+        /// <summary>
+        /// Try to connect Bluetooth Device
+        /// </summary>
+        public async Task<int> ConnectBluetooth()
+        {
+            BluetoothLEAdvertisementWatcher watcher = new BluetoothLEAdvertisementWatcher();
+            ulong btaddr = 0;
+            watcher.Received += (BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args) =>
+            {
+                if (args.Advertisement.LocalName == mainApp.DEVICE_NAME)
+                {
+                    btaddr = args.BluetoothAddress;
+                }
+            };
+            watcher.Stopped += (s, e) => {
+                mainApp.bluethoothConnected = false;
+            };
+            watcher.ScanningMode = BluetoothLEScanningMode.Active;
+            console( "Searching Device");
+            watcher.Start();
+            await Task.Delay(3000);
+            watcher.Stop();
+            if (btaddr == 0)
+            {
+                console("Device not found.");
+                return -1;
+            }
+            console( "Connecting Device");
+            BluetoothLEDevice device = await BluetoothLEDevice.FromBluetoothAddressAsync(btaddr);
+            console("Connected");
+            GattDeviceServicesResult services = await device.GetGattServicesForUuidAsync(new Guid(mainApp.SERVICE_UUID));
+            switch (services.Status)
+            {
+                case GattCommunicationStatus.Success:
+                    console("Service:Success");
+                    break;
+                case GattCommunicationStatus.AccessDenied:
+                    console("Service:AccessDenied");
+                    return -2;
+                case GattCommunicationStatus.Unreachable:
+                    console("Service:Unreachable");
+                    return -3;
+                case GattCommunicationStatus.ProtocolError:
+                    console("Service:ProtocolError");
+                    return -4;
+            }
+            GattCharacteristicsResult characteristics = await services.Services[0].GetCharacteristicsForUuidAsync(new Guid(mainApp.CHARACTERISTIC_UUID));
+            switch (characteristics.Status)
+            {
+                case GattCommunicationStatus.Success:
+                    console("Characteristic:Success");
+                    break;
+                case GattCommunicationStatus.AccessDenied:
+                    console("Characteristic:AccessDenied");
+                    return -5;
+                case GattCommunicationStatus.Unreachable:
+                    console("Characteristic:Unreachable");
+                    return -6;
+                case GattCommunicationStatus.ProtocolError:
+                    console("Characteristic:ProtocolError");
+                    return -7;
+            }
+            GattCharacteristic c = characteristics.Characteristics[0];
+            c.ValueChanged += mainApp.onValueChanged;
+            GattWriteResult result = await c.WriteClientCharacteristicConfigurationDescriptorWithResultAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
+            switch (characteristics.Status)
+            {
+                case GattCommunicationStatus.Success:
+                    console("Subscription:Success");
+                    break;
+                case GattCommunicationStatus.AccessDenied:
+                    console("Subscription:AccessDenied");
+                    return -8;
+                case GattCommunicationStatus.Unreachable:
+                    console("Subscription:Unreachable");
+                    return -9;
+                case GattCommunicationStatus.ProtocolError:
+                    console("Subscription:ProtocolError");
+                    return -10;
+            }
+            await Task.Delay(1000);
+            mainApp.bluethoothConnected = true;
+            console("Connected");
+            return 0;
+        }
+        private async void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!mainApp.bluethoothConnected)
+            {
+                connectButton.IsEnabled = false;
+                await ConnectBluetooth();
+                connectButton.IsEnabled = true;
+            }
+            else
+            {
+                connectButton.IsEnabled = false;
+                console("connected");   
+            }
+        }
+    }
+}
